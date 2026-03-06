441f37658e18f4467c95822369ff787ab28e8d00
VictorArguelho <varguelho69@gmail.com>
2026-03-05 23:42:31 -0300

    Add ScreenBuffer

---
+++ scr/ScreenUI/ScreenBuffer.cs
@@ -0,0 +1,147 @@
+using Game.Mathematics;
+
+namespace Game.ScreenUI
+{
+    public static class ScreenBuffer
+    {
+        private readonly static HashSet<Vector2Int> _dirtyCells = [];
+        private static ScreenCell[,]? _buffer = null;
+        private static bool _needsFullRender = false;
+        private static int _width;
+        private static int _height;
+
+        public static void ResizeBuffer(int width, int height)
+        {
+            _width = width;
+            _height = height;
+            _buffer = new ScreenCell[width, height];
+            ClearBuffer();
+        }
+
+        public static void ClearBuffer()
+        {
+            ResetBufferCells();
+            _needsFullRender = true;
+        }
+
+        #region SetCell overloads
+
+        public static void SetCellChr(
+            Vector2Int position,
+            char chr
+        )
+        {
+            ref var cell = ref GetCell(position);     
+
+            if(cell.SetCell(
+                    chr, 
+                    cell.ChrColor, 
+                    cell.BackgroundColor
+                )
+            )
+                _dirtyCells.Add(position);
+        }
+
+        public static void SetCellChrColor(
+            Vector2Int position,
+            ConsoleColor chrColor
+        )
+        {
+            ref var cell = ref GetCell(position);     
+
+            if(cell.SetCell(
+                    cell.Chr, 
+                    chrColor, 
+                    cell.BackgroundColor
+                )
+            )
+                _dirtyCells.Add(position);
+        }
+
+        public static void SetCellBackgroundColor(
+            Vector2Int position,
+            ConsoleColor backgroundColor
+        )
+        {
+            ref var cell = ref GetCell(position);     
+
+            if(cell.SetCell(
+                    cell.Chr, 
+                    cell.ChrColor, 
+                    backgroundColor
+                )
+            )
+                _dirtyCells.Add(position);
+        }
+
+        #endregion
+
+        public static DirtyScreenCell[] GetDirtyCells()
+        {
+            if (_needsFullRender)
+            {
+                _needsFullRender = false;
+                return GetAllCellsForRender();
+            }
+               
+            return CollectDirtyCells();
+        }
+
+        private static void ResetBufferCells()
+        {
+            if(_buffer == null)
+                throw new InvalidOperationException("Buffer não inicializado");
+
+            for(int x = 0; x < _width; x++)
+                for(int y = 0; y < _height; y++)
+                    _buffer[x, y] = new();
+        }
+
+        private static ref ScreenCell GetCell(Vector2Int position)
+        {
+            if (_buffer == null)
+                throw new InvalidOperationException("Buffer não inicializado");
+
+            if (position.X < 0 || position.X >= _width ||
+                position.Y < 0 || position.Y >= _height
+            )
+                throw new ArgumentOutOfRangeException(nameof(position));
+
+            return ref _buffer[position.X, position.Y];
+        }
+
+        private static DirtyScreenCell[] GetAllCellsForRender()
+        {
+            if(_buffer == null)
+                throw new InvalidOperationException("Buffer não inicializado");
+
+            var dirtyCells = new DirtyScreenCell[_width, _height];
+
+                for(int x = 0; x < _width; x++)
+                    for(int y = 0; y < _height; y++)
+                        dirtyCells[x, y] = new(
+                                _buffer[x, y],
+                                new(x, y)
+                            );       
+
+                return [.. dirtyCells];
+        }
+
+        private static DirtyScreenCell[] CollectDirtyCells()
+        {
+            if(_buffer == null)
+                throw new InvalidOperationException("Buffer não inicializado");
+
+            var dirty = _dirtyCells
+                .Select(pos => 
+                    new DirtyScreenCell(
+                        _buffer[pos.X, pos.Y], 
+                        pos)
+                )
+                .ToArray();
+            _dirtyCells.Clear();
+
+            return dirty;
+        }
+    }
+}
